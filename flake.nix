6b626d326314d6227a3417310381d2f05a5a1273
markupstart <mark@upstarters.com>
2026-07-15 21:54:54 -0400

    dfjfd

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
             home-manager.nixosModules.home-manager
 
             {
+              home-manager.backupFileExtension = "hm-bak";
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = { inherit inputs; };
