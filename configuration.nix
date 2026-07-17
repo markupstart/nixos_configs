9f961e4642d094c1aaa183c52abf51118d891cfa
markupstart <mark@upstarters.com>
2026-07-17 09:50:33 -0400

    pipewire settings

--- configuration.nix
+++ configuration.nix
@@ -359,9 +359,9 @@ in
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
     "context.properties" = {
       "default.clock.rate" = 48000;
-      "default.clock.quantum" = 32;
-      "default.clock.min-quantum" = 32;
-      "default.clock.max-quantum" = 32;
+      "default.clock.quantum" = 64;
+      "default.clock.min-quantum" = 64;
+      "default.clock.max-quantum" = 64;
     };
   };
 
